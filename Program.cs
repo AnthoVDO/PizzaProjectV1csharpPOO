5c08d3b631b25d3e49ae25f7773514aeef5f392c
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-10-17 17:38:40 +0200

    I continued the project and worked on the custom pizza

--- Program.cs
+++ Program.cs
@@ -6,12 +6,48 @@ using System.Text;
 namespace PizzaProjectV1csharpPOO
 {
     // Custom pizza 
-
+    // user can add his own ingredients  
+    // inherit from pizza  
+    // constructor () empty
+    // name = custom
+    // price = 5
+    // vegetarian = false
+    // ask ingredient directly to the user
+    // if empty string or null + enter the user have finished;
     class CustomPizza : Pizza
     {
-        
+        public CustomPizza()
+        {
 
+        }
+
+        public static List<string> askIngredients()
+        {
+            List<string> ingredients = new List<string>();
+            Console.WriteLine("Hello, welcome to the custom pizza generator");
+            while (true)
+            {
+                Console.Write("Please add an ingredient (PRESS ENTER WHILE FINISHED: ");
+                string ingredient = Console.ReadLine();
+                if (string.IsNullOrEmpty(ingredient))
+                {
+                    break;
+                }
+                else
+                {
+                    ingredients.Add(ingredient);
+                }
+                
+            }
+            Console.WriteLine("Thank you, your custom pizza is now available");
+            Console.WriteLine("");
+
+            return ingredients;
+
+        }
     }
+    
+
     // Create pizza class with name, price, vegetarian or no, constructor, show function
     class Pizza
     {
@@ -19,10 +55,10 @@ namespace PizzaProjectV1csharpPOO
         public float price{ get; private set; }
         public bool vegetarian { get; private set; }
         public List<string> ingredients { get; private set; }
-        public Pizza(string name, float price, bool vegetarian, List<string> ingredients)
+        public Pizza(string name = "Custom", float price = 5, bool vegetarian = false, List<string> ingredients = null)
         {
             this.name = name;
-            this.price = price ;
+            this.price = price;
             this.vegetarian = vegetarian;
             this.ingredients = ingredients;
 
@@ -57,6 +93,8 @@ namespace PizzaProjectV1csharpPOO
     }
 
     
+
+    
     class Program
     {
         
@@ -65,6 +103,11 @@ namespace PizzaProjectV1csharpPOO
         {
             // To show euro symbole
             Console.OutputEncoding = Encoding.UTF8;
+
+
+            // Ask ingredients
+
+            List<string> ingredientsForCustom = askIngredients();
             // Creating the pizza
             var pizzaList = new List<Pizza>{
                 new Pizza("Proscito", 9.5f, false, new List<string>{ "tomates", "mozzarella", "proscito"}),
@@ -72,7 +115,8 @@ namespace PizzaProjectV1csharpPOO
                 new Pizza("Carlzone", 9, false, new List<string>{"Tomates", "mozzarella", "jambon", "champignons", "oeufs", "ail" }),
                 new Pizza("Hunter", 13, false, new List<string>{"Tomates", "mozzarella", "champignons", "lardons", "oignons", "ail" }),
                 new Pizza("Hawaï", 12.5f, true, new List<string>{"Tomates", "mozzarella", "jambon", "ananas" }),
-                new Pizza("Margarita", 8.5f, true, new List<string>{"Tomates", "mozzarella" })
+                new Pizza("Margarita", 8.5f, true, new List<string>{"Tomates", "mozzarella" }),
+                new CustomPizza("Custom", 5, false, )
             };
 
             //pizzaList = pizzaList.Where(p => p.ingredients.Contains("ail")).ToList();
